57965789b18a67e2a8fff8827e86bbfa7388ca82
Lorenatacal <Lorenatacal@gmail.com>
2018-09-20 10:41:34 +0100

    Create textarea component

---
+++ src/Textarea.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Textarea extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Text Area</h3>
+            </div>
+        )
+    }
+}
+
+export default Textarea;
