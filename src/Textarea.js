4234548964886b04a81af981ab77269d3dbb274f
Lorenatacal <Lorenatacal@gmail.com>
2018-09-23 22:03:21 +0100

    Install styled-component

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const StyledTextArea = styled.div`
+    margin: 2em;
+    font-size: 1.5em;
+    text-align: left;
+`;
+
+const StyledInput = styled.input`
+    width: 600px;
+    height: 600px;
+`;
 
 class Textarea extends React.Component {
     render() {
         return (
-            <div>
+            <StyledTextArea>
                 <h3>Text Area</h3>
                 <label>
-                <input type="text" name="text" />
+                <StyledInput type="text" name="text" />
                 </label>
-            </div>
+            </StyledTextArea>
         )
     }
 }
