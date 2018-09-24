c643f28d5bb6a03e155cd2125c79adb095204c14
Lorenatacal <Lorenatacal@gmail.com>
2018-09-24 10:56:02 +0100

    Create textarea in the text component, styled text component

--- src/Textarea.js
+++ src/Textarea.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
-const StyledTextArea = styled.div`
+const StyledDiv = styled.div`
     margin: 2em;
     font-size: 1.5em;
     text-align: left;
 `;
 
-const StyledInput = styled.input`
-    width: 600px;
-    height: 600px;
+const StyledTextArea = styled.textarea`
+    width: 40%;
+    height: 350px;
+    padding: 12px 20px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    background-color: #f8f8f8;
+    resize: none;
 `;
 
-class Textarea extends React.Component {
+const StyledTitle = styled.h3`
+    text-align: left;
+    margin-left: 160px;
+`;
+
+class Text extends React.Component {
     render() {
         return (
-            <StyledTextArea>
-                <h3>Text Area</h3>
-                <label>
-                <StyledInput type="text" name="text" />
-                </label>
-            </StyledTextArea>
+            <StyledDiv>
+                <StyledTitle>Text Area</StyledTitle>
+                <StyledTextArea type="text" name="text" />
+            </StyledDiv>
         )
     }
 }
 
-export default Textarea;
+export default Text;
