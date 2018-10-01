a9d74b567116913b6337c4ba32c196e64af3638f
Lorenatacal <Lorenatacal@gmail.com>
2018-10-01 13:51:49 +0100

    Change App component in a stateless function component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Header from './Header'
 import './App.css';
 import Previewer from './Previewer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Text />
-        <Previewer />
-      </div>
-    );
-  }
+const App = function() {
+  return (
+    <div className="App">
+      <Header />
+      <Text />
+      <Previewer />
+    </div>
+  );
 }
 
 export default App;
