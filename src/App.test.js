8a6272c37e9ef125f7238ef9db9a0b35c2684ac7
Lorenatacal <lorena.tacal@gmail.com>
2018-10-06 20:01:40 +0100

    Test the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("App should render correctly", () => {
+    const wrapper = Enzyme.shallow(<App />)
+    expect(toJson(wrapper)).toMatchSnapshot();
 });
