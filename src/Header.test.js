7e88e8e941aad771c646b19f4c5de40fb77f64dc
Lorenatacal <lorena.tacal@gmail.com>
2018-10-08 13:30:33 +0100

    Test Header component

---
+++ src/Header.test.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+ 
+Enzyme.configure({ adapter: new Adapter() });
+
+test("Header should render correctly", () => {
+    const wrapper = Enzyme.shallow(<Header />)
+    expect(toJson(wrapper)).toMatchSnapshot();
+})
